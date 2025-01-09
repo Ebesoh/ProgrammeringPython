15691765f89d6419b84f779e784405669082feda
ebesoh <ebesoh82@gmail.com>
2025-01-09 15:15:31 +0100

    Function som addera två tal

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ print("This program was made by Ebesoh")
 # End of program
 # Another comment
 # No new commit
+
+# Enkelt program som adera två tal
+tal1 = float(input("skriv in tal 1: "))
+tal2 = float(input("Skrin in tal 2: "))
+summa = tal1 + tal2
+print("Summa av tal 1 och tal 2 är: ", summa )
