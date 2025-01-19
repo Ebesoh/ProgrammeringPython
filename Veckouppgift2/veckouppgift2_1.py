7908ca5c044f66e692fe7eb2569f6511fbf4e251
Ebesoh <ebesoh82@gmail.com>
2025-01-19 13:02:42 +0100

    veckouppgift2_1: code updated to calculate discount based on if customer is a member or not.

--- Veckouppgift2/veckouppgift2_1.py
+++ Veckouppgift2/veckouppgift2_1.py
@@ -10,6 +10,7 @@
 #Svar:
 # 6. Diskutera möjliga förbättringar på koden.
 #Svar:
+from operator import truediv
 
 is_member = False
 level1 = 100
@@ -17,33 +18,40 @@ level2 = 300
 discount = 0
 
 # Fel i koden eftersom om gränsvillkor uppfylla inte kravet. så länge price > level1 kommer alltid både level1&2 rabatter att exekvera och total skall 10% + 25%.
-print("\n***** Fel i koden eftersom om gränsvillkor uppfylla inte kravet. så länge price > level1 då kommer alltid både level1&2 rabatter att exekvera vilken är vilket ger rabbat på level 1 + 2 ********\n")
-price = input("Välkommen, köp något dyrt: ")
-price = float(price)
-if price > level1:
-    print("Grattis! Du har avancerat till nivå 1 och får 10% rabatt.")
-    discount = discount + 10
-if price >= level2:
-    print("Grattis! Du har avancerat till nivå 2 och får 25% rabatt.")
-    discount = discount + 25
+#print("\n***** Fel i koden eftersom om gränsvillkor uppfylla inte kravet. så länge price > level1 då kommer alltid både level1&2 rabatter att exekvera vilken är vilket ger rabbat på level 1 + 2 ********\n")
+#price = input("Välkommen, köp något dyrt: ")
+#price = float(price)
+#if price > level1:
+#   print("Grattis! Du har avancerat till nivå 1 och får 10% rabatt.")
+#    discount = discount + 10
+#if price >= level2:
+#    print("Grattis! Du har avancerat till nivå 2 och får 25% rabatt.")
+#    discount = discount + 25
 
-final_price = price * (100 - discount)/100
-print("Efter rabatter blir priset ......." + str(final_price))
+#final_price = price * (100 - discount)/100
+#print("Efter rabatter blir priset ......." + str(final_price))
 
-## Rätt som uppfyller gränsvillkor kravet 25% rabbat när price är >= level 2 och 10% rabbat när price är större än level 1 men mindre än level 3
+
 is_member = False
 level1 = 100
 level2 = 300
 discount = 0
-print("\n***** Rätt kod som uppfyller gränsvillkor uppfylla i kravet: 25% rabbat när price är >= level 2 och 10% rabbat när price är större än level 1 men mindre än level 3  ********\n")
-price = input("Välkommen, köp något dyrt: ")
-price = float(price)
-if level2 > price >= level1:
-    print("Grattis! Du har avancerat till nivå 1 och får 10% rabatt.")
-    discount = discount + 10
-if price >= level2:
-    print("Grattis! Du har avancerat till nivå 2 och får 25% rabatt.")
-    discount = discount + 25
 
+price = float(input("Välkommen, köp något dyrt: "))
+medlem = input(" Är du medlem? 'ja' eller 'nej':")
+if medlem == 'ja':
+    is_member = True
+else:
+    is_member = False
+
+if is_member :
+    if level2 > price >= level1:
+        print("Grattis! Du har avancerat till nivå 1 och får 10% rabatt.")
+        discount = discount + 10
+        if price >= level2:
+            print("Grattis! Du har avancerat till nivå 2 och får 25% rabatt.")
+            discount = discount + 25
+else:
+    print("")
 final_price = price * (100 - discount)/100
 print("Efter rabatter blir priset ......." + str(final_price))
